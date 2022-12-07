53ab9acf4274c5ca81d6600246113ef78630fff7
Aseem Chopra <aseemchopra@protonmail.com>
2022-12-07 23:09:02 +0530

    Updates

---
+++ precache-manifest.8eedf95768eb829ad8e45e19c071eb11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6263f74eff94613334ac69d6f92dc7d9",
+    "url": "/shagun/index.html"
+  },
+  {
+    "revision": "c0b6627cc22c4839f7eb",
+    "url": "/shagun/static/css/main.31b6137c.chunk.css"
+  },
+  {
+    "revision": "282a523bc2fed708944f",
+    "url": "/shagun/static/js/2.5d396f03.chunk.js"
+  },
+  {
+    "revision": "e1261e3d2e863b33c03d34d175bc3ed6",
+    "url": "/shagun/static/js/2.5d396f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b6627cc22c4839f7eb",
+    "url": "/shagun/static/js/main.32ea63ea.chunk.js"
+  },
+  {
+    "revision": "3d130c6be06fbfdeac92",
+    "url": "/shagun/static/js/runtime-main.611d1687.js"
+  },
+  {
+    "revision": "de28f71ec6eb8dfda2e68d2211ee49eb",
+    "url": "/shagun/static/media/AvenirNextLTPro-Demi.de28f71e.otf"
+  }
+]);
